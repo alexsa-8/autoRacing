a5bfc9aa8a3342577b5b62fdf20de3ec31c7f738
Alex <alexsa-8@mail.ru>
2022-10-07 20:04:40 +0300

    Я сделал класс Driver абстрактным, а для водителей с разными категориями - сделать конкретные классы. Но в main показывает ошибку на созданных объектах. Я не могу понять в чём причина?
    Driver<PassengerCars> alex = new DriverB<>("Alex", "B", 3);
    В этой строке подчеркивает угловые скобки в DriverB<>
    
    Помоги разобраться.

---
+++ src/DriverB.java
@@ -0,0 +1,28 @@
+import AutoRacing.Car;
+
+public class DriverB extends Driver<Car> {
+    public DriverB(String naimDriver, String driverLicenseCategory, int experience) {
+        super(naimDriver, driverLicenseCategory, experience);
+    }
+
+    @Override
+    public void controlMachine(Car Car) {
+        System.out.println("Водитель "+getNaimDriver()+" управляет автомобилем "+Car.getMark()+" и будет участвовать в заезде");
+    }
+
+    @Override
+    public void startMoving(Car Car) {
+        System.out.println("Стартует 100 км/ч за " + Car.getStartMoving() + " сек.");
+    }
+
+    @Override
+    public void stop(Car Car) {
+        System.out.println("Останавлисается при скорости 100 км/ч за " + Car.getFinishMovement() + " сек.");
+    }
+
+    @Override
+    public void refuelCar(Car Car) {
+        System.out.println("Заправляет авто "+Car.getPitstop()+" раз.");
+    }
+
+}
